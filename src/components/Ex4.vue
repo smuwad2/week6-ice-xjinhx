f88b721dc4fa49c79d64d607b373fce86f54ffa7
He Jinhong <jinhong.he.2024@computing.smu.edu.sg>
2025-10-01 07:21:03 +0000

    updated exercises

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,59 +1,43 @@
 <script>
-    export default { 
-        data() {
-            return {
-                // Add code here
-                id : "demo1",
-                blueBox : true,
-                blueBtn : true
-            }
-        },
-        methods: { 
-   			// Add code here
-            //toChange(isBlue){
-                // if (isBlue){
-                //    console.log(isBlue);
-                //    this.isBlue=false;
-                //    isBlue=false;
-                //    this.color="redBox";
-                //}
-                //else{
-                //    this.isBlue=true;
-                //    isBlue=false;
-                //    this.color="blueBox";
-                //}
-
-            //}
-
-            changeColor(){
-                this.blueBox=!this.blueBox;
-                this.redBox=!this.bl
-            }
-		}
+export default { 
+    data() {
+        return {
+            id : "demo1",
+            isBlue : true  
+        }
+    },
+    methods: { 
+        changeColor(){
+            this.isBlue = !this.isBlue;
+        }
     }
+}
 </script>
 
 <template>
-    
-    <!-- Modify code here -->
-    <div   class="container">
-        <div v-bind:id="id" class="m-2" :class="{'blueBox' : blueBox,'redBox' : !blueBtn}">
+    <div class="container">
+        <div :id="id" class="m-2" :class="{ blueBox: isBlue, redBox: !isBlue }">
             div ID : {{id}} 
         </div>
         
-        <button type="button" :class="{'btn btn-primary' : blueBtn, 'btn btn-danger' : !blueBtn}" v-on:click = "changeColor()">Change Color</button>
+        <button 
+            type="button" 
+            :class="{ 'btn btn-primary': isBlue, 'btn btn-danger': !isBlue }" 
+            @click="changeColor"
+        >
+            Change Color
+        </button>
     </div>
-
 </template>
 
 <style scoped>
-    .redBox {
-        background-color: red;
-    }
-    .blueBox {
-        background-color: blue;
-    }
-    #demo1 {
-        color: white;
-    }
+.redBox {
+    background-color: red;
+}
+.blueBox {
+    background-color: blue;
+}
+#demo1 {
+    color: white;
+}
 </style>
